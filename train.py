f547d9f7840aaf440a3c16acb71ae7076dd2ee40
Koral <62809012+KoralK5@users.noreply.github.com>
2021-11-24 08:41:32 -0500

    Update and rename depressionAI.py to train.py

--- train.py
+++ train.py
@@ -39,18 +39,3 @@ for epoch in range(epochs):
 f = open('model.pkl', 'wb')
 pickle.dump(nlp, f)
 f.close()
-
-f = open('model.pkl', 'rb')
-nlpModel = pickle.load(f)
-categories = nlpModel.get_pipe('textcat')
-
-message = ' '
-while message != '':
-    message = input('- ')
-    docs = [nlpModel.tokenizer(message)]
-
-    scores = categories.predict(docs)[0]
-    prediction = categories.labels[scores.argmax()]
-    prediction = ('❌😞 bad' if prediction=='suicide' else '✔️😊 good') + ' mental health'
-
-    print(f'\nPrediction: {prediction} with a certainty of {int(max(scores)*100)}%\n\n')
